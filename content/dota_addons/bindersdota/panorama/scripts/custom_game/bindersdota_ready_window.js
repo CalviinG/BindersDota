7382196395ed5cbb5df71e4f75b1a674f8e28b5f
Dan Andersson <danandersson92@gmail.com>
2015-09-27 11:31:32 +0200

    Began creating the HUD

---
+++ content/dota_addons/bindersdota/panorama/scripts/custom_game/bindersdota_ready_window.js
@@ -0,0 +1,7 @@
+function CreateReadyWindow() {
+  //CustomUI.DynamicHud_Create( -1, ReadyWindow, bindersdota_ready_window.xml, nil)
+}
+
+function ForceReady() {
+  $.Msg(" ***** ForceReady() ***** ");
+}
