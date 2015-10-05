7dcf994649f2d79b3e0a16ef01cbd6f8d9507406
Alexander Hult <alexander.hult14@gmail.com>
2015-10-05 23:35:33 +0200

    Added Celsius second and third spell, updated Hero list further.

---
+++ game/dota_addons/bindersdota/scripts/vscripts/Abilities/celsius_absolute_zero.lua
@@ -0,0 +1,21 @@
+function apply_debuff(params)
+
+	local hero_base = params.target:FindModifierByName("modifier_absolute_zero_base")
+	local hero_stack = params.target:FindModifierByName("modifier_absolute_zero_stack")
+
+	-- Case 1: Target is a hero. Target does not have the base debuff.
+	-- In this case, apply the base debuff and a stack debuff
+	if params.target:IsHero() and hero_base == nil then
+		params.ability:ApplyDataDrivenModifier(params.caster, params.target, "modifier_absolute_zero_base", nil)
+		params.ability:ApplyDataDrivenModifier(params.caster, params.target, "modifier_absolute_zero_stack", nil)
+		params.target:FindModifierByName("modifier_absolute_zero_stack"):IncrementStackCount()
+
+	-- Case 2: Target is a hero. Target has the base debuff.
+	-- In this case, increment the stack count if needed and reset the durations on both the base and stack debuffs.
+	elseif params.target:IsHero() and hero_base ~= nil then
+		-- Only update the stack count if we are below what's defined as the maximum number.
+		if hero_stack:GetStackCount() < params.max_stacks then hero_stack:IncrementStackCount() end
+		hero_base:SetDuration(params.debuff_duration, true)
+		hero_stack:SetDuration(params.debuff_duration, true)
+	end
+end
